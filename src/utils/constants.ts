2a946d520ced3f2fd1c9a33cd05425e0c43190eb
amdonatusprince <donatusprince@gmail.com>
2023-12-06 09:42:44 +0100

    contract address fixed

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,10 +3,10 @@ import { AddrMapType, MapType } from './types';
 
 export const registryAddress: AddrMapType = {
   0: '0x0',
-  1442: '0x4838854e5150e4345fb4ae837e9fcca40d51f3fe'
+  1440002: '0xA2A3b38f6088d729a1454BCD2863ce87B9953079'
   
 }
 
 export const explorer: MapType = {
-  1442: 'testnet-zkevm.polygonscan.com',
+  1440002: 'evm-sidechain.xrpl.org',
 }
