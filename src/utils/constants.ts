72f67017dd3a67d8e26cff7870375a46a98a296b
amdonatusprince <donatusprince@gmail.com>
2023-12-06 09:19:42 +0100

    feat: verxio XRP init

---
+++ src/utils/constants.ts
@@ -0,0 +1,12 @@
+
+import { AddrMapType, MapType } from './types';
+
+export const registryAddress: AddrMapType = {
+  0: '0x0',
+  1442: '0x4838854e5150e4345fb4ae837e9fcca40d51f3fe'
+  
+}
+
+export const explorer: MapType = {
+  1442: 'testnet-zkevm.polygonscan.com',
+}
